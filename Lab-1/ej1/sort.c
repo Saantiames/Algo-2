1eec69b49d553edbb50b9c1b841c27a0e6b6e2f4
Santiago Ames <santi.ames2004@gmail.com>
2025-04-19 17:19:47 -0300

    Ej 1. Insertion sort - Completed

--- Lab-1/ej1/sort.c
+++ Lab-1/ej1/sort.c
@@ -8,11 +8,21 @@
 
 
 static void insert(int a[], unsigned int i, unsigned int length) {
-    /* needs implementation */
+    //insertion sort: 
+    //compare one by one until it finds a smaller element, then swap
+    array_dump(a,length);
+    for(unsigned int j = i; j>0 ; --j){
+        if(goes_before(a[j],a[j-1])){
+            swap(a,j-1,j);
+        }
+    }
 }
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1; i < length; ++i) {
         insert(a, i, length);
+        //Invariant control
+        assert(array_is_sorted(a,i));
+        printf("A is sorted until A[%d]: \n",i-1);
     }
 }
